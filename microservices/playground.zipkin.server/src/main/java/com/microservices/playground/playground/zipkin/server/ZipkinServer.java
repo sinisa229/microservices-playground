99cbda40124bd99f84dbf088541bf00d2a685ce7
Sinisha Mihajlovski <sinisha.mihajlovski@ixaris.com>
2018-07-12 11:50:48 +0200

    Configuring Zipkin

--- microservices/playground.zipkin.server/src/main/java/com/microservices/playground/playground/zipkin/server/ZipkinServer.java
+++ microservices/playground.zipkin.server/src/main/java/com/microservices/playground/playground/zipkin/server/ZipkinServer.java
@@ -2,7 +2,9 @@ package com.microservices.playground.playground.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServer {
 
--- microservices/playground.zipkin.server/start.sh
+++ microservices/playground.zipkin.server/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar zipkin-web-all.jar -zipkin.web.port=:9412 -zipkin.web.rootUrl=/ -zipkin.web.query.dest=lalhost:9411
+java -jar zipkin-web-all.jar -zipkin.web.port=:9412 -zipkin.web.rootUrl=/ -zipkin.web.query.dest=localhost:9411
